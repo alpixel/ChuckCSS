6bd77d25b47b6d021a9827ebc482a8c577a27521
Mickael SURREL <mickael@alpixel.fr>
2016-02-24 18:02:54 +0100

    Add Block helpers

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,14 @@ module.exports = function(grunt) {
         files: {
           "dist/chuckcss.min.css": "chuckcss/front.less",
           "dist/bootstrap-override.min.css": "chuckcss/bootstrap/bootstrap-override.less",
-          "dist/print.css": "chuckcss/media/print.less"
+          "dist/print.css": "chuckcss/print.less"
         }
       },
       development: {
         files: {
           "dist/bootstrap.css": "chuckcss/bootstrap/bootstrap-override.less",
           "dist/chuckcss.css": "chuckcss/front.less",
-          "dist/print.css": "chuckcss/media/print.less"
+          "dist/print.css": "chuckcss/print.less"
         }
       }
     },
